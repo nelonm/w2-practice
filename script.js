92bd6130167d702cca1df512a22f8d9b64fffc97
nelonm <acsigy@gmail.com>
2023-09-22 20:06:57 +0200

    flex practice

--- script.js
+++ script.js
@@ -18,3 +18,27 @@ for (let index = 0; index < array.length; index++) {
     rootElement.insertAdjacentHTML('beforeend', `<h2> ${array[index]}</h2>`);
     } */
 
+fetch('https://restcountries.com/v3.1/all').then(function(response){
+    return(response.json())
+    .then(function(data){
+        console.log(data) // itt tudunk a kapott adattal dolgozni
+                                    // a data egy 250 elemből álló array
+
+         for (let index = 0; index < data.length; index++) {
+            let countryName =data[index].name.common
+            let countryPop = data[index].population
+            let countryArea = data[index].area
+
+
+            rootElement.insertAdjacentHTML(`beforeend`, `
+            <div class="country"> 
+                <h2>${countryName}</h2>
+                <p> population: ${countryPop}</p>
+                <p> area: ${countryArea} km2</p>
+            </div>
+            `)
+            
+         }                           
+
+    })
+})
