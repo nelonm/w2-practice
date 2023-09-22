ba3318ca4b523668f9c89e758a61f1680cfbe8aa
nelonm <acsigy@gmail.com>
2023-09-22 18:38:49 +0200

    for loop

--- homework.js
+++ homework.js
@@ -28,3 +28,48 @@ function squaring (z) {
 squaring (124)
 
 answer (squaring (124))
+
+
+// homework Wednesday
+
+let myArray = ["blue", "red", "green", "yellow"]
+
+let myObject = {
+    color: "blue",
+    shape: "round",
+    weight: "0,5 kg"
+}
+
+console.log (myArray[3])
+console.log(myObject.shape)
+
+let myMusic = {
+    "favorite": "pop",
+    other: ["irish", "classical", "instrumental"],
+    singer: {
+        name: "Taylor Swift",
+        cd: ["Red", "1989", "Fearless", "Folklore"],
+        songs: [
+            {
+                title: "22", 
+                album: "1989"
+            },
+            {
+                title: "Betty",
+                album: "Folklore"
+            },
+            {
+                title: "All too well",
+                album: "Red"
+            }
+        ]
+    },
+    band: {
+        name: "Onerepublic",
+        albums: ["Dreaming out loud", "Native"],
+        songs: ["Apologize", "Angel of Mercy", "Come home", "Lose myself"]
+    }
+}
+
+console.log (myMusic.other[2])
+console.log(myMusic.singer.songs[0].title)
